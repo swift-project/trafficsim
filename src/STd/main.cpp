4833f4a2b6e499385f37ea7be1720735cba93d5a
Peter Buchegger <peter@pbuchegger.at>
2014-02-02 17:55:28 +0100

    #112 project settings changed, default startup settings

--- src/STd/main.cpp
+++ src/STd/main.cpp
@@ -3,7 +3,7 @@
  *  License, v. 2.0. If a copy of the MPL was not distributed with this
  *  file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-//#define VATSIM_GERMANY_TEST
+#define VATSIM_GERMANY_TEST
 
 // QtArg include.
 #include <QtArg/Arg>
